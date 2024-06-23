3fb7673c8221225fc091d7b2cab2c81a2a6bbd39
clean17 <piw940317@gmail.com>
2024-06-24 02:42:07 +0900

    v2

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def delete_video(filename):
     return '', 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8090)
 
