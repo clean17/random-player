17bf15313b2dd2cb16cca582e48f91ad974013ec
clean17 <piw940317@gmail.com>
2024-06-29 01:49:44 +0900

    ãmake Thumbnail

--- task_manager.py
+++ task_manager.py
@@ -18,9 +18,10 @@ class Task:
         self.thumbnail_path = None
         self.thumbnail_update_time = None
         # self.creation_time = None
-        self.initial_thumbnail_created = False
-
         self.creation_time = datetime.now()  # 예시로 현재 시간을 사용
+        self.initial_thumbnail_created = False
+        self.thumbnail_duration = 0;
+
 
     def update_last_modified(self):
         latest_file = self.get_latest_file()
@@ -39,41 +40,36 @@ class Task:
         return latest_file
 
     def generate_thumbnail(self):
+        current_time = datetime.now()
+
         if self.file_name and self.creation_time:
             if not self.initial_thumbnail_created:
                 # 최초 썸네일 생성 (파일 시작 1초 후)
-                initial_thumbnail_path = os.path.join(self.work_directory, self.file_name.replace('.ts', '_0s.jpg'))
+                initial_thumbnail_path = os.path.join(self.work_directory, self.file_name.replace('.ts', '_thumb.jpg'))
                 initial_cmd = f"ffmpeg -y -i {os.path.join(self.work_directory, self.file_name)} -ss 1 -vframes 1 {initial_thumbnail_path}"
-                print(f"Executing command: {initial_cmd}")
-                result = subprocess.run(initial_cmd, shell=True)
-                print(f"Command result: {result.returncode}")
+                result = subprocess.run(initial_cmd, shell=True, capture_output=True, text=True, encoding='utf-8')
                 if result.returncode == 0:
                     self.thumbnail_path = initial_thumbnail_path
                     self.initial_thumbnail_created = True
-                    print(f"Initial thumbnail created at {self.thumbnail_path}")
+                    self.thumbnail_update_time = current_time
+                    # print(f"Initial thumbnail created at {self.thumbnail_path}")
                 else:
                     print("Failed to create initial thumbnail")
 
-            current_time = datetime.now()
-            # 이후 썸네일은 파일 생성 시간으로부터 매 분마다 생성
-            if not self.thumbnail_update_time or (current_time - self.thumbnail_update_time).seconds >= 60:
-                thumbnail_time = self.creation_time + timedelta(seconds=60 * (self.creation_time.minute + 1))  # 첫 번째 썸네일은 생성 후 1분
-                print(f"########################################### {thumbnail_time} ##############################################")
-                while thumbnail_time < current_time:
-                    thumbnail_path = os.path.join(self.work_directory, f"{self.file_name.replace('.ts', '')}_{thumbnail_time.minute}.jpg")
-                    cmd = f"ffmpeg -y -i {os.path.join(self.work_directory, self.file_name)} -ss {thumbnail_time.minute * 60} -vframes 1 {thumbnail_path}"
-                    print(f"Executing command: {cmd}")
-                    result = subprocess.run(cmd, shell=True)
-                    print(f"Command result: {result.returncode}")
+            elif self.thumbnail_update_time:
+                # 이후 썸네일은 마지막 썸네일 생성 시간으로부터 매 분마다 생성
+                if (current_time - self.thumbnail_update_time).seconds >= 5:
+                    self.thumbnail_duration += 5
+                    thumbnail_path = os.path.join(self.work_directory, f"{self.file_name.replace('.ts', '')}_thumb.jpg")
+                    cmd = f"ffmpeg -y -i {os.path.join(self.work_directory, self.file_name)} -ss {self.thumbnail_duration} -frames:v 1 {thumbnail_path}"
+                    # print(f"Executing command: {cmd}")
+                    result = subprocess.run(cmd, shell=True, capture_output=True, text=True, encoding='utf-8')
                     if result.returncode == 0:
                         self.thumbnail_path = thumbnail_path
-                        print(f"Thumbnail created at {self.thumbnail_path}")
+                        self.thumbnail_update_time = current_time
                     else:
-                        print(f"Failed to create thumbnail at {thumbnail_time.minute} minute mark")
-                    thumbnail_time += timedelta(minutes=1)
+                        print(f"Failed to create thumbnail at {self.thumbnail_duration} minute mark")
 
-                self.thumbnail_update_time = current_time
-                print(f"Updated thumbnail update time to {self.thumbnail_update_time}")
     @staticmethod
     def terminate(pid):
         """특정 PID와 그 자식 프로세스를 강제로 종료합니다."""
@@ -95,7 +91,7 @@ def update_task_status():
     while True:
         for task in tasks:
             task.update_last_modified()
-        time.sleep(3)
+        time.sleep(5)
 
 # 스레드 시작
 threading.Thread(target=update_task_status, daemon=True).start()
