ff189304f3a5964e715edb232e8f87f56fae9371
clean17 <piw940317@gmail.com>
2024-06-30 18:48:38 +0900

    no image, check clipboard

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ def signal_handler(sig, frame):
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     # app.run(debug=True, host='0.0.0.0', port=8090)
-    app.run(debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
-'''
+    # app.run(debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+
     # Create a logger
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
@@ -83,5 +83,5 @@ if __name__ == '__main__':
     # Add the handlers to the logger
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-'''
-    # serve(app, host='0.0.0.0', port=8090)
+
+    serve(app, host='0.0.0.0', port=8090) ## ssl > nginx 에 적용
