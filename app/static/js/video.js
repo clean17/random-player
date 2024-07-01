c14087376f411dc5cd5ac9ce90b7333a1b788e46
clean17 <piw940317@gmail.com>
2024-07-02 03:15:54 +0900

    seperate video, css, js

---
+++ app/static/js/video.js
@@ -0,0 +1,267 @@
+function setVideoOptions(vodUrl, videoFileType) {
+    let videoOptions = {
+        sources: [
+            {
+                src: vodUrl,
+                type: videoFileType
+            }
+        ],
+        controls: true, // 동영상 제어를 위한 컨트롤 바 제공 여부
+        playsinline: true, // 웹 브라우저 환경의 재생 형태
+        muted: false, // 최초 재생시 무음인지
+        preload: "auto", // 비디오 데이터를 즉시 다운로드 시작할 지 여부
+        controlBar: {
+            playToggle: true, // 재생, 일시정지 토글
+            pictureInPictureToggle: true, // pip모드
+            remainingTimeDisplay: true, // 남은 시간 표시
+            progressControl: true // 재생 진행바
+        },
+    };
+    return videoOptions;
+}
+
+function delVideo() {
+    if (currentVideo) {
+        if (confirm("Are you sure?")) {
+            videoPlayer.pause();
+            videoPlayer.src = '';
+            videoPlayer.load();
+
+            axios.delete(`/video/delete/${encodeURIComponent(currentVideo)}?directory=${directory}`)
+                .then(response => {
+                    if (response.status === 204) {
+                        currentVideo = '';
+                        getVideo();
+                    } else {
+                        alert('Failed to delete video');
+                    }
+                }).catch(err => {
+                alert(currentVideo)
+            });
+        }
+    }
+}
+
+const hideControls = () => {
+    clearTimeout(hideControlsTimeout);
+    hideControlsTimeout = setTimeout(() => {
+        controls.style.display = 'none';
+        topDiv.style.display = 'none';
+    }, 2000);
+};
+
+const showControls = () => {
+    controls.style.display = 'block';
+    topDiv.style.display = 'block';
+    hideControls();
+};
+
+document.getElementById('nextButton').removeEventListener('click', getVideo);
+document.getElementById('nextButton').addEventListener('click', getVideo);
+document.getElementById('deleteButton').removeEventListener('click', delVideo);
+document.getElementById('deleteButton').addEventListener('click', delVideo);
+
+videoPlayer.addEventListener('play', showControls);
+videoPlayer.addEventListener('pause', showControls);
+videoPlayer.addEventListener('click', showControls);
+videoPlayer.addEventListener('touchstart', showControls);
+videoPlayer.addEventListener('ended', getVideo);
+
+videoPlayer.addEventListener('mousemove', showControls);
+videoPlayer.addEventListener('touchmove', showControls);
+
+
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
+
+
+function showSyncMessage() {
+    syncMessage.textContent = 'Audio Sync Offset: ' + audioOffset.toFixed(2) + 's';
+    syncMessage.style.display = 'block';
+    clearTimeout(syncMessage.hideTimeout);
+    syncMessage.hideTimeout = setTimeout(function() {
+        syncMessage.style.display = 'none';
+    }, 2000); // 2초 후 메시지 숨기기
+}
+
+function adjustAudioSync(offset) {
+    audioOffset += offset;
+    var videoElement = player.el().querySelector('video');
+    var currentTime = videoElement.currentTime;
+    videoElement.currentTime = currentTime + offset;
+    showSyncMessage();
+}
+
+function resetAudioSync() {
+    audioOffset = 0;
+    showSyncMessage();
+}
+
+function showVolumeMessage() {
+    volumeMessage.textContent = 'Volume: ' + Math.round(player.volume() * 100) + '%';
+    volumeMessage.style.display = 'block';
+    clearTimeout(volumeMessage.hideTimeout);
+    volumeMessage.hideTimeout = setTimeout(function() {
+        volumeMessage.style.display = 'none';
+    }, 2000); // 2초 후 메시지 숨기기
+}
+
+function exitFullscreen() {
+    if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
+function toggleFullscreen() {
+    if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.mozFullScreenElement && !document.msFullscreenElement) {  // 현재 전체화면이 아닌 경우
+        var docEl = document.documentElement;
+        if (docEl.requestFullscreen) {
+            docEl.requestFullscreen();
+        } else if (docEl.webkitRequestFullscreen) {
+            docEl.webkitRequestFullscreen();
+        } else if (docEl.mozRequestFullScreen) {
+            docEl.mozRequestFullScreen();
+        } else if (docEl.msRequestFullscreen) {
+            docEl.msRequestFullscreen();
+        }
+    } else {  // 현재 전체화면인 경우
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
+function addKeyboardControls() {
+    document.addEventListener('keydown', function(event) {
+        var currentTime = player.currentTime();
+        var duration = player.duration();
+
+        switch(event.key) {
+            case 'ArrowRight':
+                if (event.shiftKey) {
+                    player.currentTime(Math.min(currentTime + 30, duration));
+                } else {
+                    player.currentTime(Math.min(currentTime + 5, duration));
+                }
+                break;
+            case 'ArrowLeft':
+                if (event.shiftKey) {
+                    player.currentTime(Math.max(currentTime - 30, 0));
+                } else {
+                    player.currentTime(Math.max(currentTime - 5, 0));
+                }
+                break;
+            case 'ArrowUp':
+                player.volume(Math.min(player.volume() + 0.1, 1));
+                showVolumeMessage();
+                break;
+            case 'ArrowDown':
+                player.volume(Math.max(player.volume() - 0.1, 0));
+                showVolumeMessage();
+                break;
+            case 'a':  // 'a' 키를 눌러 오디오 싱크를 -0.02초 조정
+                adjustAudioSync(-0.02);
+                break;
+            case 'd':  // 'd' 키를 눌러 오디오 싱크를 +0.02초 조정
+                adjustAudioSync(0.02);
+                break;
+            case 's':  // 's' 키를 눌러 오디오 싱크를 0으로 초기화
+                resetAudioSync();
+                break;
+            case 'Delete':  // 'Delete' 키를 눌러 비디오 삭제 함수 호출
+                delVideo();
+                break;
+            case 'PageDown':  // 'PageDown' 키를 눌러 비디오 가져오기 함수 호출
+                getVideo();
+                break;
+            case ' ':  // 스페이스바를 눌러 재생/일시정지 토글
+                event.preventDefault();  // 스페이스바의 기본 동작 방지
+                if (player.paused()) {
+                    player.play();
+                } else {
+                    player.pause();
+                }
+                break;
+            case 'Escape':  // ESC 키를 눌러 전체화면 해제
+                exitFullscreen();
+                break;
+            case 'Enter':
+                toggleFullscreen();
+                break;
+        }
+    })
+}
+
+// TODO 이벤트만 붙이면 작동할것 같다
+function threeSplitLayout() {
+    var videoElement = player.el().querySelector('video');
+    var videoRatio = videoElement.videoHeight / videoElement.videoWidth;
+    if (videoRatio > 1) {
+        var videoContainer = document.getElementById('videoContainer');
+        var videoPlayer = document.getElementById('videoPlayer');
+        // 왼쪽 비디오 추가
+        videoLeft = document.createElement('video');
+        videoLeft.id = 'videoLeft';
+        videoLeft.className = 'video-mirror video-js vjs-default-skin';
+        videoLeft.muted = 'true'
+        videoLeft.autoplay = 'true'
+        videoLeft.setAttribute('loop', '');
+        videoContainer.insertBefore(videoLeft, videoPlayer);
+
+        // 오른쪽 비디오 추가
+        videoRight = document.createElement('video');
+        videoRight.id = 'videoRight';
+        videoRight.className = 'video-mirror video-js vjs-default-skin';
+        videoRight.muted = 'true'
+        videoRight.autoplay = 'true'
+        videoRight.setAttribute('loop', '');
+        videoContainer.appendChild(videoRight);
+
+        let mainSrc = videoPlayer.querySelector('source').src;
+
+        videoLeft.src = mainSrc;
+        videoRight.src = mainSrc;
+        videoLeft.load();
+        videoRight.load();
+
+        function playVideo() {
+            videoLeft.play();
+            videoRight.play();
+        }
+
+        function pauseVideo() {
+            video.pause();
+        }
+
+        function skipForward() {
+            if (video.duration - video.currentTime > 10) {
+                video.currentTime += 10;
+            } else {
+                video.currentTime = video.duration; // Move to the end of the video if less than 10s remain
+            }
+        }
+
+        function skipBackward() {
+            if (video.currentTime > 10) {
+                video.currentTime -= 10;
+            } else {
+                video.currentTime = 0; // Go back to the start if less than 10s into the video
+            }
+        }
+    }
+}
