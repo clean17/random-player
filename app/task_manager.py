7b1061a98f0d8a3547b1a431861370c15e903c96
clean17 <piw940317@gmail.com>
2024-07-06 15:59:43 +0900

    fix processing 1

--- app/task_manager.py
+++ app/task_manager.py
@@ -6,16 +6,31 @@ import os
 import glob
 import subprocess
 from datetime import datetime, timedelta
-import multiprocessing
+from multiprocessing import Process, Manager
 import logging
 from concurrent.futures import ThreadPoolExecutor
 
 tasks = []
+running_processes = []
 
-# scheduler = sched.scheduler(time.time, time.sleep) # sched 기본 스케줄러, 블로킹
+# sched 기본 스케줄러, 블로킹
+# scheduler = sched.scheduler(time.time, time.sleep)
 
 # 스케줄리 인스턴스 생성 (논블로킹)
 scheduler = BackgroundScheduler()
+
+'''
+    # 1. multiprocessing는 메모리를 공유하지 않는다 -> class의 필드를 공유하지 않음
+    # process = multiprocessing.Process(target=self.generate_thumbnail)
+    # process.start() 
+
+    # 2. 스레드를 무한히 생성하는 오류
+    #thread = threading.Thread(target=self.generate_thumbnail) 
+    #thread.start()
+
+    # 3. 자식 프로세스가 무한히 증식한다..
+    #self.executor.submit(self.generate_thumbnail) 
+'''
 
 class Task:
     def __init__(self, pid, file_pattern, work_directory):
@@ -23,6 +38,7 @@ class Task:
         self.file_pattern = file_pattern
         self.work_directory = work_directory
         self.last_modified_time = None
+        self.last_checked_time = None
         self.file_name = None
         self.thumbnail_path = None
         self.thumbnail_update_time = None
@@ -34,58 +50,34 @@ class Task:
     def update_last_modified(self):
         latest_file = self.get_latest_file()
         if latest_file:
-            # print('################################ ', latest_file, self.thumbnail_update_time)
             self.file_name = os.path.basename(latest_file)
-            self.last_modified_time = datetime.fromtimestamp(os.path.getmtime(latest_file)).strftime('%Y-%m-%d %H:%M:%S')
             self.creation_time = datetime.fromtimestamp(os.path.getctime(latest_file))
+            last_modified_time = datetime.fromtimestamp(os.path.getmtime(latest_file))
+            if self.last_checked_time is None or last_modified_time != self.last_checked_time:
+                self.last_modified_time = last_modified_time.strftime('%Y-%m-%d %H:%M:%S')
+                self.last_checked_time = last_modified_time
+                # print(f"######### Updated ######### : {self.file_name} - - {self.thumbnail_update_time} - - {self.last_modified_time}")
+                self.set_param_generate_thumbnail()
 
-            params = {
-                'file_name': self.file_name,
-                'work_directory': self.work_directory,
-                'initial_thumbnail_created': self.initial_thumbnail_created,
-                'thumbnail_path': self.thumbnail_path,
-                'thumbnail_update_time': self.thumbnail_update_time,
-                'last_modified_time': self.last_modified_time,
-                'creation_time': self.creation_time,
-            }
+    def set_param_generate_thumbnail(self):
+        params = {
+            'file_name': self.file_name,
+            'work_directory': self.work_directory,
+            'initial_thumbnail_created': self.initial_thumbnail_created,
+            'thumbnail_path': self.thumbnail_path,
+            'thumbnail_update_time': self.thumbnail_update_time,
+            'last_modified_time': self.last_modified_time,
+            'creation_time': self.creation_time,
+        }
 
-            # multiprocessing.Manager > 공유 dict
-            manager = multiprocessing.Manager()
-            return_dict = manager.dict()
+        # multiprocessing.Manager > 공유 dict
+        manager = Manager()
+        return_dict = manager.dict()
+        process = Process(target=self.generate_thumbnail, args=(params, return_dict))
+        process.start()
 
-            process = multiprocessing.Process(target=self.generate_thumbnail, args=(params, return_dict))
-            process.start()
-            process.join()
-
-            # 프로세스가 완료된 후 공유된 dict에서 결과를 가져옴
-            self.thumbnail_path = return_dict.get('thumbnail_path')
-            self.thumbnail_update_time = return_dict.get('thumbnail_update_time')
-            self.initial_thumbnail_created = return_dict.get('initial_thumbnail_created')
-            print(f"Updated thumbnail_path: {self.thumbnail_path}")
-            print(f"Updated thumbnail_update_time: {self.thumbnail_update_time}")
-
-            # process = multiprocessing.Process(target=self.generate_thumbnail)
-            # process.start() # 1. multiprocessing는 메모리를 공유하지 않는다 -> class의 필드를 공유하지 않음
-
-            #thread = threading.Thread(target=self.generate_thumbnail) # 2. 스레드를 무한히 생성하는 오류
-            #thread.start()
-
-            #self.executor.submit(self.generate_thumbnail) # 3. 자식 프로세스가 무한히 증식한다..
-
-    #def get_latest_file(self):
-        # 파일 수정 시간이 딜레이 되는 이슈
-        # files = glob.glob(self.file_pattern)
-        #if not files:
-        #    return None
-
-        #latest_file = max(files, key=os.path.getctime)
-        #return latest_file
-        #try:
-        #    files = [os.path.join(self.work_directory, f) for f in os.listdir(self.work_directory) if self.file_pattern in f]
-        #    latest_file = max(files, key=os.path.getmtime)
-        #    return latest_file
-        #except ValueError:
-        #    return None
+        # 프로세스를 리스트에 추가하여 추적
+        running_processes.append((self, process, return_dict))
 
     def get_latest_file(self):
         search_pattern = os.path.join(self.work_directory, self.file_pattern)
@@ -94,10 +86,12 @@ class Task:
         if not files:
             return None
 
+        # latest_file = max(files, key=os.path.getctime) # 수정시간 딜레이 이슈
         latest_file = max(files, key=os.path.getmtime)
         return latest_file
 
     def generate_thumbnail(self, params, return_dict):
+        print('## call generate_thumbnail')
         file_name = params['file_name']
         work_directory = params['work_directory']
         initial_thumbnail_created = params['initial_thumbnail_created']
@@ -111,11 +105,12 @@ class Task:
                 # 최초 썸네일 생성 (파일 시작 1초 후)
                 initial_thumbnail_path = os.path.join(work_directory, file_name.replace('.ts', '_thumb.jpg'))
                 initial_cmd = f"ffmpeg -y -i {os.path.join(work_directory, file_name)} -ss 1 -vframes 1 -s 426x240 -q:v 10 {initial_thumbnail_path}"
-                result = subprocess.run(initial_cmd, shell=True, capture_output=True, text=True, encoding='utf-8')
+                result = subprocess.run(initial_cmd, shell=True, capture_output=True, text=True, encoding='utf-8', check=True)
                 if result.returncode == 0:
                     thumbnail_path = initial_thumbnail_path
                     initial_thumbnail_created = True
                     thumbnail_update_time = datetime.now().isoformat()
+                    print('## first thumnail ok')
                 else:
                     print("Failed to create initial thumbnail. Error: {result.stderr}")
 
@@ -126,15 +121,16 @@ class Task:
                 modification_time = datetime.strptime(last_modified_time, '%Y-%m-%d %H:%M:%S')
                 duration = modification_time - creation_time
                 thumb_duration = duration.total_seconds()
-
                 thumb_time_difference = (current_time - last_update_time).total_seconds()
-                if thumb_time_difference >= 60:
+
+                if thumb_time_difference >= 15:
                     thumbnail_path = os.path.join(work_directory, f"{file_name.replace('.ts', '')}_thumb.jpg")
                     cmd = f"ffmpeg -y -i {os.path.join(work_directory, file_name)} -ss {int(thumb_duration)} -frames:v 1 -s 426x240 -q:v 10 {thumbnail_path}"
-                    result = subprocess.run(cmd, shell=True, capture_output=True, text=True, encoding='utf-8')
+                    result = subprocess.run(cmd, shell=True, capture_output=True, text=True, encoding='utf-8', check=True)
                     if result.returncode == 0:
                         thumbnail_path = thumbnail_path
                         thumbnail_update_time = datetime.now().isoformat()
+                        print(f"Created Thumbnail at {file_name} {thumb_duration} second mark.")
                     else:
                         print(f"Failed to create thumbnail at {thumb_duration} second mark. Error: {result.stderr}")
 
@@ -157,23 +153,59 @@ class Task:
 def current_date():
     return datetime.now().strftime('%y%m%d')
 
+# 이미 종료된 프로세스는 목록에서 제거
 def cleanup_tasks():
     global tasks
     current_time = datetime.now()
-    threshold_time = timedelta(minutes=20)  # 20분
+    threshold_time = timedelta(minutes=11)  # 11분
     format_str = '%Y-%m-%d %H:%M:%S'
-    tasks = [task for task in tasks if current_time - datetime.strptime(task.last_modified_time, format_str) < threshold_time]
+    new_tasks = []
+
+    for task in tasks:
+        if task.last_modified_time is None:
+            print(f"Task {task.file_name} has no last_modified_time. Skipping.")
+            new_tasks.append(task)  # last_modified_time이 없으면 유지
+            continue
+
+        task_time = datetime.strptime(task.last_modified_time, format_str)
+        time_difference = current_time - task_time
+        print(f"Task {task.file_name}: last_modified_time = {task.last_modified_time}, current_time = {current_time}, time_difference = {time_difference}")
+
+        if time_difference < threshold_time:
+            new_tasks.append(task)  # 11분 미만이면 유지
+
+    tasks[:] = new_tasks
+    print(f"Remaining tasks: {len(tasks)}")
+
+
 
 # 작업 상태를 주기적으로 업데이트하는 스레드
 def update_task_status():
     for task in tasks:
         task.update_last_modified()
 
+def check_running_processes():
+    global running_processes
+    for entry in running_processes[:]:  # 리스트 복사본을 사용하여 안전하게 반복
+        if len(entry) != 3:
+            print(f"Skipping entry with unexpected format: {entry}")
+            continue
+        task, process, return_dict = entry
+        if not process.is_alive():
+            process.join()  # 프로세스가 종료되었으므로 리소스 해제
+            if 'thumbnail_path' in return_dict:
+                task.thumbnail_path = return_dict['thumbnail_path']
+                task.thumbnail_update_time = return_dict['thumbnail_update_time']
+                task.initial_thumbnail_created = return_dict['initial_thumbnail_created']
+                print(f"Thumbnail updated at {task.thumbnail_update_time} for file {task.file_name}")
+            running_processes.remove(entry)
+
 # 스레드 시작 (썸네일 생성이 늘어진다..?)
 # threading.Thread(target=update_task_status, daemon=True).start()
 
-# 스케줄러에 작업 추가
-scheduler.add_job(update_task_status, 'interval', seconds=15)
-scheduler.add_job(cleanup_tasks, 'interval', minutes=5)
+# 스케줄러에 작업 추가, max_instances 기본 1
+scheduler.add_job(update_task_status, 'interval', seconds=10, max_instances=1)
+scheduler.add_job(cleanup_tasks, 'interval', minutes=1)
+scheduler.add_job(check_running_processes, 'interval', seconds=5)
 
 scheduler.start()
