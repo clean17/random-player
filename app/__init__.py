5024d50a8ee77c8d3675ebd7a7eef83dcb4b230e
clean17 <piw940317@gmail.com>
2024-07-29 22:14:37 +0900

    mod move_image event

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import os
 from datetime import datetime
-from flask import Flask, session
+from flask import Flask, session, send_file, render_template_string
 from flask_login import LoginManager
 from .auth import auth, User, users
 from config import load_config
 from .ffmpeg_handle import m_ffmpeg
 from .main import main
 from .video import video
+from .image import image_bp, environment
 
 def create_app():
     app = Flask(__name__, static_folder='static')
@@ -17,6 +18,8 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(video, url_prefix='/video')
     app.register_blueprint(m_ffmpeg, url_prefix='/ffmpeg')
+    app.register_blueprint(image_bp)
+    app.jinja_env.globals.update(max=max, min=min)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
@@ -36,6 +39,23 @@ def create_app():
         if user_id in users:
             return User(user_id)
         return None
+
+    @app.route('/logs')
+    def view_logs():
+        try:
+            return send_file('logs/app.log', mimetype='text/plain')
+        except Exception as e:
+            return str(e)
+
+    @app.route('/logs_html')
+    def view_logs_html():
+        try:
+            with open('logs/app.log', 'r', encoding='utf-8') as log_file:
+                log_content = log_file.read()
+            log_html = '<br>'.join(log_content.split('\n'))
+            return render_template_string('<pre>{{ logs }}</pre>', logs=log_html)
+        except Exception as e:
+            return str(e)
 
     def check_server_restarted():
         restart_flag = 'server_status.txt'
