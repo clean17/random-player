ce9288678accf2a38f45bfd32cf7605fcd19f9e6
clean17 <piw940317@gmail.com>
2025-05-12 17:43:04 +0900

    jwt test

---
+++ utils/jwt.py
@@ -0,0 +1,22 @@
+import jwt
+from datetime import datetime, timedelta
+from config.config import settings
+
+SECRET_KEY = settings['SECRET_KEY']
+ALGORITHM = "HS256"
+TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decoded
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
