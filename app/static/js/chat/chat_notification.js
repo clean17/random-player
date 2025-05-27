7b31c20513f96b34a527caf8776c5200527b8fa3
clean17 <piw940317@gmail.com>
2025-05-28 00:46:45 +0900

    modify chat_notification.js

--- app/static/js/chat/chat_notification.js
+++ app/static/js/chat/chat_notification.js
@@ -3,14 +3,21 @@ let userInteracted = false;
 // 도메인 구입 전까지 서비스 워커 기능 비활성화 > PWA 기능 or FCM 추가해야함
 if ("serviceWorker" in navigator) {
     // http + localhost, https(공인 ssl) 환경에서만 기동
-    navigator.serviceWorker.register('/service-worker.js?v=2').then(registration => {
+    /*navigator.serviceWorker.register('/service-worker.js?v=2').then(registration => {
         return registration.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: "BM3Xq3X-hbmCtGvoJv3Dl-WmW1nTYenl4tKQtE4pdcMTK0XDxjrECQSmtFgnPd1aqUoBINRCKrLqfqwIdemSXZs" // YOUR_PUBLIC_VAPID_KEY
         });
     }).then(subscription => {
         console.log("Push Subscription:", JSON.stringify(subscription));
-    });
+
+        return fetch("/subscribe", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(subscription)
+        });
+    }).catch(error => console.error("푸시 구독 실패:", error));
+    ;*/
 }
 
 function requestNotificationPermission() {
@@ -46,10 +53,10 @@ function sendNotification(data) {
     if (document.hidden && Notification.permission === "granted") {
         if (!isMine && !isUnderline) {
             navigator.serviceWorker.ready.then(registration => { // 서비스 워커 알림
-                registration.showNotification("새 메시지 도착!", {
+                registration.showNotification("새 알림", {
                     // body: `${data.username}: ${data.msg}`,
-                    icon: "/static/favicon.ico",
-                    badge: "/static/favicon.ico",
+                    // icon: "/static/favicon.ico", // 메인 블록 우측 큰 이미지
+                    badge: "/static/favicon.ico", // 상단 헤더 뱃지
                     vibrate: [200, 100, 200],  // 진동 패턴 (안드로이드)
                 });
             });
@@ -95,25 +102,3 @@ document.body.removeEventListener('touchmove', requestNotificationPermission);
 document.body.addEventListener('touchmove', requestNotificationPermission);
 document.body.removeEventListener('click', requestNotificationPermission);
 document.body.addEventListener('click', requestNotificationPermission);
-
-// 클라이언트에서 서버에 푸시 구독 정보 전송
-/*if ("serviceWorker" in navigator && "PushManager" in window) {
-    navigator.serviceWorker.register("/service-worker.js")
-        .then(registration => {
-            console.log("서비스 워커 등록 완료:", registration);
-
-            return registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: "YOUR_PUBLIC_VAPID_KEY"
-            });
-        })
-        .then(subscription => {
-            console.log("푸시 구독 성공:", subscription);
-            return fetch("/subscribe", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(subscription)
-            });
-        })
-        .catch(error => console.error("푸시 구독 실패:", error));
-}*/
