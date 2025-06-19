141da02d1e992f45d31bea6e25adb3d916dfd74e
clean17 <piw940317@gmail.com>
2025-06-19 12:25:11 +0900

    temp

--- utils/jwt.py
+++ utils/jwt.py
@@ -8,7 +8,7 @@ TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
