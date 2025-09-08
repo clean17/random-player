1f03c6d65d955f6bb1cc679df15884c0a4ebf9bc
clean17 <piw940317@gmail.com>
2025-09-08 09:51:31 +0900

    Adding a toss api request function

---
+++ utils/request_toss_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+INFO_URL = "https://wts-info-api.tossinvest.com/api/v3/search-all/wts-auto-complete"
+OVERVIEW_URL = "https://wts-info-api.tossinvest.com/api/v2/stock-infos/PRODUCTCODE/overview"
+def request_stock_info_with_toss_api(stock_name):
+    payload = {
+        "query": stock_name,
+        "sections": [
+            {"type": "SCREENER"},
+            {"type": "PRODUCT", "option": {"addIntegratedSearchResult": "true"}},
+            {"type": "TICS"}
+        ]
+    }
+    headers = {"Content-Type": "application/json"}
+
+    res = requests.post(INFO_URL, headers=headers, data=json.dumps(payload))
+    return res.json()
+
+def request_stock_overview_with_toss_api(product_code):
+    REPLACE_URL = OVERVIEW_URL.replace('PRODUCTCODE', product_code);
+    res = requests.get(REPLACE_URL)
+    return res.json()
