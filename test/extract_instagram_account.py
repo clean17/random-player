80d28a7f8a1c8a5a8d5b3d5e327ecfd6e73d3de7
clean17 <piw940317@gmail.com>
2025-09-21 15:11:58 +0900

    Modifying extract_instagram_account.py

--- test/extract_instagram_account.py
+++ test/extract_instagram_account.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 
+# 주어진 링크에서 계정명만 추출
 data = """
 
 """
@@ -18,3 +19,29 @@ for line in data.strip().splitlines():
 
 print(usernames)
 # ['aaaaaaaaaaa', 'bbbbbbbbbbb']
+
+
+
+
+
+
+
+
+
+
+
+
+from pathlib import Path
+
+BASE = Path(r"C:\Users\piw94\Pictures\dir_name")  # r''로 백슬래시 이스케이프 방지
+
+# 폴더명 리스트 (상위 경로 바로 아래만)
+folder_names = sorted([p.name for p in BASE.iterdir() if p.is_dir()], key=str.casefold)
+
+# 디렉토리의 하위 디렉토리명을 배열로 리턴
+print(folder_names)
+
+
+"""
+['추출됨']
+"""
