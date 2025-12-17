aafd72a6183b1b53da499282a1781c445c3c91de
clean17 <piw940317@gmail.com>
2025-12-18 01:24:58 +0900

    Modify batch_process.py

--- job/batch_process.py
+++ job/batch_process.py
@@ -31,13 +31,13 @@ def renew_kiwoom_token_job():
 
     # subprocess 실행 (새로운 프로세스), subprocess.Popen()은 어느 스레드에서 호출하든 OS에 “새 프로세스 생성”을 요청
     process = subprocess.Popen(
-        [venv_python, py_script],
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
         cwd=r"C:\my-project\random-player",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
         stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
         stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
         text=True,
-        encoding="cp949",
-        errors="ignore",                      # 디코딩 안되는 문자 무시
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
         bufsize=1
     )
 
@@ -50,125 +50,19 @@ def renew_kiwoom_token_job():
 
 def run_crawl_ai_image():
     print('    ############################### run_crawl_image ###############################')
-    # # 명령어 조합
-    # # Windows에서는 여러 명령을 &&로 연결하여 한 줄에 실행 가능
-    # # venv 활성화 후 바로 실행
-    # script_dir = r'C:\my-project\random-player'
-    # venv_activate = r'venv\Scripts\activate'
-    # py_script = r'python utils\crawl_image_by_playwright.py'
-    #
-    # # 전체 명령어 (venv 활성화 → 스크립트 실행)
-    # # 주의: activate.bat는 cmd에서만 인식, powershell은 다름
-    # cmd = f'cmd /c "cd /d {script_dir} && {venv_activate} && {py_script} && exit"'
-
     venv_python = r"C:\my-project\random-player\venv\Scripts\python.exe"
-    py_script = r"C:\my-project\random-player\utils\crawl_image_by_playwright.py"
-
-    # subprocess 실행
-    process = subprocess.Popen(
-        [venv_python, py_script],
-        cwd=r"C:\my-project\random-player",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-        encoding="cp949",
-        errors="ignore"
-    )
-    stdout, stderr = process.communicate(timeout=60)
-
-
-'''
-cd /d C:\my-project\AutoSales.py
-venv\Scripts\activate
-python multi_kor_stocks.py
-'''
-def predict_stock_graph(stock):
-    print(f'    ############################### predict_stock_graph : {stock} ###############################')
-    script_dir = r'C:\my-project\AutoSales.py'
-    venv_activate = r'venv\Scripts\activate'
-    if stock == 'kospi':
-        py_script = r'python multi_kor_stocks.py'
-    if stock == 'nasdaq':
-        py_script = r'python new_nasdaq_multi.py'
-
-    # 전체 명령어 (venv 활성화 → 스크립트 실행)
-    # 주의: activate.bat는 cmd에서만 인식, powershell은 다름
-    cmd = f'cmd /c "cd /d {script_dir} && {venv_activate} && {py_script} && exit"'
-
-    # subprocess 실행
-    process = subprocess.Popen(
-        cmd,
-        # stdout=subprocess.PIPE, # 버퍼가 꽉 차서 죽는다 ? > 서브프로세스(=실행된 명령)의 표준출력(stdout)이 '파이썬 부모 프로세스'로 파이프로 전달
-        # stderr=subprocess.PIPE,
-        text=True,
-        shell=True,
-        encoding='utf-8',
-        errors="ignore" # 디코딩 안되는 문자 무시
-    )
-    # stdout, stderr = process.communicate(timeout=60) # 버퍼를 읽어줘야 죽지 않는다
-    # print("STDOUT:", stdout)
-    # print("STDERR:", stderr)
-
-def find_stocks():
-    script_dir = r'C:\my-project\AutoSales.py'
-    venv_activate = r'venv\Scripts\activate'
-    py_script = r'python 2_finding_stocks_with_increased_volume.py'
-
-    cmd = f'cmd /c "cd /d {script_dir} && {venv_activate} && {py_script} && exit"'
-
-    # subprocess 실행
-    process = subprocess.Popen(
-        cmd,
-        text=True,
-        shell=True,
-        encoding='utf-8',
-        errors="ignore" # 디코딩 안되는 문자 무시
-    )
-
-def find_low_stocks():
-    script_dir = r'C:\my-project\AutoSales.py'
-    venv_activate = r'venv\Scripts\activate'
-    py_script = r'python 4_find_low_point.py'
-
-    cmd = f'cmd /c "cd /d {script_dir} && {venv_activate} && {py_script} && exit"'
-
-    # subprocess 실행
-    process = subprocess.Popen(
-        cmd,
-        text=True,
-        shell=True,
-        encoding='utf-8',
-        errors="ignore" # 디코딩 안되는 문자 무시
-    )
-
-def update_interest_stocks():
-    # script_dir = r'C:\my-project\AutoSales.py'
-    # venv_activate = r'venv\Scripts\activate'
-    # py_script = r'python 1_periodically_update_today_interest_stocks.py'
-    #
-    # cmd = f'cmd /c "cd /d {script_dir} && {venv_activate} && {py_script} && exit"'
-    #
-    # # subprocess 실행
-    # process = subprocess.Popen(
-    #     cmd,
-    #     text=True,
-    #     shell=True,
-    #     encoding='utf-8',
-    #     errors="ignore" # 디코딩 안되는 문자 무시
-    # )
-
-    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
-    py_script = r"C:\my-project\AutoSales.py\1_periodically_update_today_interest_stocks.py"
+    # py_script = r"C:\my-project\random-player\utils\scrap_ai_by_playwright.py"
+    py_script = r"C:\my-project\random-player\utils\scrap_ai_by_playwright_async.py"
 
     # subprocess 실행 (새로운 프로세스)
     process = subprocess.Popen(
-        [venv_python, py_script],
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
         cwd=r"C:\my-project\random-player",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
         stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
         stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
         text=True,
-        encoding="cp949",
-        errors="ignore",                      # 디코딩 안되는 문자 무시
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
         bufsize=1
     )
 
@@ -178,3 +72,127 @@ def update_interest_stocks():
     process.wait()
     print("returncode =", process.returncode)
 
+
+'''
+cd /d C:\my-project\AutoSales.py
+venv\Scripts\activate
+python multi_kor_stocks.py
+'''
+def predict_stock_graph(stock):
+    print(f'    ############################### predict_stock_graph : {stock} ###############################')
+    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
+    if stock == 'kospi':
+        py_script = r"C:\my-project\AutoSales.py\multi_kor_stocks.py"
+    if stock == 'nasdaq':
+        py_script = r"C:\my-project\AutoSales.py\new_nasdaq_multi.py"
+
+    # subprocess 실행 (새로운 프로세스)
+    process = subprocess.Popen(
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
+        cwd=r"C:\my-project\AutoSales.py",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
+        stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
+        stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
+        text=True,
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
+        bufsize=1
+    )
+
+    for line in process.stdout:
+        print(line, end="")   # 실시간 콘솔 출력
+
+    process.wait()
+    print("returncode =", process.returncode)
+
+
+def update_interest_stocks():
+    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
+    py_script = r"C:\my-project\AutoSales.py\1_periodically_update_today_interest_stocks.py"
+
+    # subprocess 실행 (새로운 프로세스)
+    process = subprocess.Popen(
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
+        cwd=r"C:\my-project\AutoSales.py",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
+        stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
+        stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
+        text=True,
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
+        bufsize=1
+    )
+
+    for line in process.stdout:
+        print(line, end="")   # 실시간 콘솔 출력
+
+    process.wait()
+    if process.returncode != 0:
+        print("returncode =", process.returncode)
+
+
+def find_stocks():
+    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
+    py_script = r"C:\my-project\AutoSales.py\2_finding_stocks_with_increased_volume.py"
+
+    # subprocess 실행 (새로운 프로세스)
+    process = subprocess.Popen(
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
+        cwd=r"C:\my-project\AutoSales.py",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
+        stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
+        stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
+        text=True,
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
+        bufsize=1
+    )
+
+    for line in process.stdout:
+        print(line, end="")   # 실시간 콘솔 출력
+
+    process.wait()
+    print("returncode =", process.returncode)
+
+
+def find_low_stocks():
+    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
+    py_script = r"C:\my-project\AutoSales.py\4_find_low_point.py"
+
+    # subprocess 실행 (새로운 프로세스)
+    process = subprocess.Popen(
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
+        cwd=r"C:\my-project\AutoSales.py",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
+        stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
+        stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
+        text=True,
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
+        bufsize=1
+    )
+
+    for line in process.stdout:
+        print(line, end="")   # 실시간 콘솔 출력
+
+    process.wait()
+    print("returncode =", process.returncode)
+
+
+def update_stocks_daily():
+    venv_python = r"C:\my-project\AutoSales.py\venv\Scripts\python.exe"
+    py_script = r"C:\my-project\AutoSales.py\update_kor_stocks_periodically.py"
+
+    # subprocess 실행 (새로운 프로세스)
+    process = subprocess.Popen(
+        [venv_python, "-X", "utf8", py_script],  #  UTF-8 강제하면 이모지 출력 가능
+        cwd=r"C:\my-project\AutoSales.py",   # 자식 프로세스의 현재 작업 디렉토리(working directory) 를 지정
+        stdout=subprocess.PIPE,               # 주석하면 자식 프로세스의 출력이 “파이프로 캡처되지 않고” 그냥 기본 출력 스트림으로 흘러간다
+        stderr=subprocess.STDOUT,             # stderr도 stdout으로 합치기(편함)
+        text=True,
+        encoding="utf-8",                     # 부모도 UTF-8로 읽기
+        errors="replace",                     # ignore 대신 replace 추천(문제 보이게)
+        bufsize=1
+    )
+
+    for line in process.stdout:
+        print(line, end="")   # 실시간 콘솔 출력
+
+    process.wait()
+    print("returncode =", process.returncode)
