fd4e70d56dd768c45b8e197ac000659a77301b52
clean17 <piw940317@gmail.com>
2026-06-30 18:59:38 +0900

    fix: 서버 시작 후 문제 발생 시 node 프로세스도 종료

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import signal
 import subprocess
 # from flask_cors import CORS
 from config.config import settings
-from utils.common import signal_handler, register_shutdown_handlers
+from utils.common import signal_handler, register_shutdown_handlers, cleanup
 from job.batch_runner import initialize_directories, create_scheduler
 
 NODE_SERVER_PATH = settings['NODE_SERVER_PATH']
@@ -33,31 +33,34 @@ if __name__ == '__main__':
     # 종료 핸들러
     register_shutdown_handlers(scheduler, node_process)
 
-    if select_server == 0: # werkzeug, 개발
-        from utils.wsgi_midleware import logger
-        from werkzeug.middleware.proxy_fix import ProxyFix
-        logger.info("############################### Starting server.... ####################################")
-        from app import create_app # Flask, # create_app 에서 WebSocket 기능을 추가함
-        app = create_app()
-        # 실제 클라이언트 IP (X-Forwarded-For) 를 읽도록
-        app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
-        app.run(debug=True, host='0.0.0.0', port=8088, use_reloader=True, threaded=True)
-        # app.run(debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'), threaded=True)
+    try:
+        if select_server == 0: # werkzeug, 개발
+            from utils.wsgi_midleware import logger
+            from werkzeug.middleware.proxy_fix import ProxyFix
+            logger.info("############################### Starting server.... ####################################")
+            from app import create_app # Flask, # create_app 에서 WebSocket 기능을 추가함
+            app = create_app()
+            # 실제 클라이언트 IP (X-Forwarded-For) 를 읽도록
+            app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
+            app.run(debug=True, host='0.0.0.0', port=8088, use_reloader=True, threaded=True)
+            # app.run(debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'), threaded=True)
 
-    if select_server == 1: # waitress, 운영
-        from waitress import serve
-        from utils.wsgi_midleware import RequestLoggingMiddleware, HopByHopHeaderFilter, ReverseProxied, logger
-        logger.info("############################### Starting server.... ####################################")
-        from app import create_app
-        app = create_app()
+        if select_server == 1: # waitress, 운영
+            from waitress import serve
+            from utils.wsgi_midleware import RequestLoggingMiddleware, HopByHopHeaderFilter, ReverseProxied, logger
+            logger.info("############################### Starting server.... ####################################")
+            from app import create_app
+            app = create_app()
 
-        # Hop-by-Hop 헤더 필터 미들웨어 적용
-        app.wsgi_app = HopByHopHeaderFilter(app.wsgi_app)
-        # 커스텀 로깅 설정 미들웨어 적용
-        app.wsgi_app = RequestLoggingMiddleware(app.wsgi_app)
-        # 모든 요청에 대해 URL 스킴(scheme)을 강제로 HTTPS로 설정, 리버스 프록시 환경에서도 클라이언트 요청을 HTTPS로 인식하여 보안 기능 동작하도록 함
-        app.wsgi_app = ReverseProxied(app.wsgi_app)
+            # Hop-by-Hop 헤더 필터 미들웨어 적용
+            app.wsgi_app = HopByHopHeaderFilter(app.wsgi_app)
+            # 커스텀 로깅 설정 미들웨어 적용
+            app.wsgi_app = RequestLoggingMiddleware(app.wsgi_app)
+            # 모든 요청에 대해 URL 스킴(scheme)을 강제로 HTTPS로 설정, 리버스 프록시 환경에서도 클라이언트 요청을 HTTPS로 인식하여 보안 기능 동작하도록 함
+            app.wsgi_app = ReverseProxied(app.wsgi_app)
 
-        # serve(app, host='0.0.0.0', port=8090, threads=12, max_request_body_size=1024*1024*1024*50)  # Waitress 서버, SSL 설정은 nginx에서 처리한다 / WebSocket 미지원, 50GB
-        # nginx 프록시 서버만 접근 허용
-        serve(app, host='127.0.0.1', port=8090, threads=12, max_request_body_size=1024*1024*1024*50)  # Waitress 서버, SSL 설정은 nginx에서 처리한다 / WebSocket 미지원, 50GB
+            # serve(app, host='0.0.0.0', port=8090, threads=12, max_request_body_size=1024*1024*1024*50)  # Waitress 서버, SSL 설정은 nginx에서 처리한다 / WebSocket 미지원, 50GB
+            # nginx 프록시 서버만 접근 허용
+            serve(app, host='127.0.0.1', port=8090, threads=12, max_request_body_size=1024*1024*1024*50)  # Waitress 서버, SSL 설정은 nginx에서 처리한다 / WebSocket 미지원, 50GB
+    finally:
+        cleanup(scheduler, node_process)
